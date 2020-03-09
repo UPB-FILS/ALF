1d6336b51441df9eae727d9cd2192e07e4d76cc1
Diana Ghindaoanu <diana_ghindaoanu@yahoo.com>
2020-03-09 20:28:13 +0200

    TP4

---
+++ TP/TP4/main.js
@@ -0,0 +1,35 @@
+"use strict";
+
+var Parser = require ('./alf.js').Parser;
+ 
+// use the lexer from the generated Parser
+var lexer = new Parser ().lexer;
+ 
+// add a text to the lexer
+lexer.setInput ("example");
+ 
+var s = null;
+ 
+// repeat while you get an EOF token
+do 
+{
+    try
+    {
+        // get the next token
+        s = lexer.next ();
+    }
+    catch (e)
+    {
+        // if there is an error
+        // print the line number (lexer.yylineno+1)
+        // print the error position (lexer.showPosition ())
+        console.log ('Error line '+(lexer.yylineno+1)+'\n'+lexer.showPosition());
+        process.exit (-1);
+    }
+    if (s && s!== lexer.EOF)
+    {
+        // print the token (s)
+        // print the token text (lexer.yytext)
+        console.log (s + ' ' + lexer.yytext);
+    }
+} while (s !== lexer.EOF); 
